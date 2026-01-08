76096a7f97a0ee012c96d758183008796543bcb7
Nora Bertholome <nora.bertholome@gmail.com>
2026-01-08 15:55:55 +0100

    Import Allure Vitest setup

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -1,6 +1,7 @@
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import 'allure-vitest/setup';
 
 expect.extend(matchers);
 
